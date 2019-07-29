115b94a07bab66b5d19f1db3a81c82dbe245aeb7
johadi <jim.hadi10@gmail.com>
2019-07-29 20:22:45 +0100

    Create influencer interface

---
+++ src/interfaces/Influencer.ts
@@ -0,0 +1,15 @@
+export interface InfluencerI {
+  id: number;
+  igFollow: string;
+  igID: string;
+  fbID: string;
+  fbFollow: string;
+  ytFollow: string;
+  ytID: string;
+  fullName: string;
+  email: string;
+  phone: string;
+  ownPromocode: string;
+  refPromoCode: string;
+  refName: string;
+}
